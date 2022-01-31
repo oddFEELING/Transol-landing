d214addbf57487ad21c35884b08b1e6a26bef269
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2022-01-31 12:06:24 +0100

    another real

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Content from '../sections/__about/content_section/COntent';
+import Content from '../sections/__about/content_section/Content';
 import Hero from '../sections/__about/hero_section/Hero';
 import Team from '../sections/__about/team_section/Team';
 import * as S from '../styles/About.style';
